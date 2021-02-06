2a73f20d261a7b91c868a2a151b5045cffc83671
svetosh <70635136+svetosh@users.noreply.github.com>
2021-02-06 14:20:41 +0300

    Update ConsoleApplication7.cpp

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -1,140 +1,92 @@
 #include <iostream>
-#include <ctime>
 #include <vector>
+
 using namespace std;
 
-void print_vec(vector<int>& vec)
+void Print(vector<int>& v)
 {
-	for (int i : vec) {
-		cout << i << " ";
-	}
-	cout << endl;
+    for (auto el : v) {
+        cout << el << " ";
+    }
+    cout << endl;
 }
 
-void bubble(vector<int>& vec, int size)
+void Buble(vector<int>& v)
 {
-	for (int i = 1; i < size; ++i)
-	{
-		for (int j = 0; j < size - i; j++)
-		{
-			if (vec[j] < vec[j + 1])
-			{
-				int fist = vec[j];
-				vec[j] = vec[j + 1];
-				vec[j + 1] = fist;
-			}
-		}
-	}
+    for (int i = 0; i < v.size() - 1; i++)
+        for (int j = 0; j < v.size() - 1; j++)
+            if (v[j] < v[j + 1])
+                swap(v[j], v[j + 1]);
 }
 
-void selection(vector<int>& vec, int size)
-{
-	int naim = 0;
-	for (int i = 0; i < size; i++)
-	{
-		naim = i;
-		for (int j = i + 1; j < size; j++)
-			naim = (vec[j] < vec[naim]) ? j : naim;
-		if (i != naim)
-		{
-			int first = vec[i];
-			vec[i] = vec[naim];
-			vec[naim] = first;
-		}
-	}
+void Selec(vector<int>& v) {
+    int min;
+    for (int i = 0; i < v.size() - 1; ++i) 
+    {
+        min = i;
+        for (int j = i + 1;j < v.size();++j)
+            if (v[j] < v[min])
+                swap(v[j], v[min]);
+    }
 }
 
-void Merg(vector<int>& vec, int begin, int end)
+void Merge(vector<int>& v, int start, int end)
 {
-	long int i = begin,
-		mid = begin + (end - begin) / 2,
-		j = mid + 1,
-		k = 0;
-		vector<int> b = vec;
-
-	while (i <= mid && j <= end) {
-		if (vec[i] <= vec[j]) {
-			b[k] = vec[i]; i++;
-		}
-		else {
-			b[k] = vec[j]; j++;
-		}
-		k++;
-	}
-
-	while (i <= mid)
-	{
-		b[k] = vec[i];
-		i++;
-		k++;
-	}
-
-	while (j <= end)
-	{
-		b[k] = vec[j];
-		j++;
-		k++;
-	}
-
-	for (i = 0; i < k; i++)
-		vec[begin + i] = b[i];
-}
-
-void MergSort(vector<int>& vec, int left, int right)
-{
-	int first = 0;
-	if (left < right)
-		if (right - left == 1) {
-			if (vec[left] > vec[right]) {
-				first = vec[left];
-				vec[left] = vec[right];
-				vec[right] = first;
-			}
-		}
-		else {
-			MergSort(vec, left, left + (right - left) / 2);
-			MergSort(vec, left + (right - left) / 2 + 1, right);
-			Merg(vec, left, right);
-		}
-
+    if (end - start < 2)
+        return;
+    if (end - start == 2)
+    {
+        if (v[start] > v[start + 1])
+            swap(v[start], v[start + 1]);
+        return;
+    }
+    Merge(v, start, start + (end - start) / 2);
+    Merge(v, start + (end - start) / 2, end);
+    vector<int> merge;
+    int mergeStart = start;
+    int mid = start + (end - start) / 2;
+    int midStrart = mid;
+    while (merge.size() < end - start)
+    {
+        if (mergeStart >= mid || (midStrart < end && v[midStrart] <= v[mergeStart]))
+        {
+            merge.push_back(v[midStrart]);
+            midStrart++;
+        }
+        else
+        {
+            merge.push_back(v[mergeStart]);
+            mergeStart++;
+        }
+    }
+    for (int i = start; i < end; i++)
+        v[i] = merge[i - start];
 }
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	vector<int> mainvec;
-	int size;
-	cout << "Введите длину массива: " << endl;
-	cin >> size;
-	cout << "Массив для сортировки: ";
-	srand(time(nullptr));
-	for (int i = 0; i < size; ++i) {
-		mainvec.push_back(rand() % 201 - 100);
-	}
-	print_vec(mainvec);
-	cout << endl
-		<< "Массив отсортированный Bubble'ом: ";
-	bubble(mainvec, mainvec.size());
-	print_vec(mainvec);
-	cout << endl
-		<< "Массив отсортированный selection'ом: ";
-	selection(mainvec, mainvec.size());
-	print_vec(mainvec);
-	cout << endl;
+    setlocale(LC_ALL, "ru");
 
-	vector<int> vec2;
-	int size2;
-	cout << "Введите длину массива, который будет сортироваться Mergе'ом: " << endl;
-	cin >> size2;
-	cout << "Массив для сортировки: ";
-	srand(time(nullptr));
-	for (int i = 0; i < size2; ++i) {
-		vec2.push_back(rand() % 201 - 100);
-	}
-	print_vec(vec2);
-	cout << endl
-		<< "Массив отсортированный Mergе'ом: ";
-	MergSort(vec2, 0, vec2.size() - 1);
-	print_vec(vec2);
-	return 0;
+    std::cout << "Hello World!\n";
+    vector<int> v;
+    int size;
+    cout << "Введите длину массива: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+        v.push_back(10 + rand() % 50);
+    Print(v);
+
+    cout << "-------------------Buble------------------" << endl;
+    Buble(v);
+    Print(v);
+    cout << "-------------------Selec------------------" << endl;
+    Selec(v);
+    Print(v);
+    cout << "-------------------Merge------------------" << endl;
+    vector<int> v2;
+    for (int i = 0; i < size; i++)
+        v2.push_back(10 + rand() % 50);
+    Print(v2);
+    Merge(v2, 0, v2.size());
+    Print(v2);
 }
